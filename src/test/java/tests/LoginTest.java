b23ec5cf20a59a31f4f4f9fc9e8fa80e9b2e6a8a
kameshsaripaka <saripaka.kamesh@gmail.com>
2026-08-15 12:57:38 +0530

    Initial Selenium TestNG project

---
+++ src/test/java/tests/LoginTest.java
@@ -0,0 +1,55 @@
+package tests;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class LoginTest {
+
+    private WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() {
+
+        driver = new ChromeDriver();
+
+        driver.manage().window().maximize();
+
+        driver.get("https://the-internet.herokuapp.com/login");
+    }
+
+    @Test
+    public void validLoginTest() {
+
+        driver.findElement(By.id("username"))
+                .sendKeys("tomsmith");
+
+        driver.findElement(By.id("password"))
+                .sendKeys("SuperSecretPassword!");
+
+        driver.findElement(By.cssSelector("button[type='submit']"))
+                .click();
+
+        String message =
+                driver.findElement(By.id("flash"))
+                        .getText();
+
+        Assert.assertTrue(
+                message.contains("You logged into a secure area!"),
+                "Login was not successful"
+        );
+    }
+
+    @AfterMethod
+    public void tearDown() {
+
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
